030176aefed737be83cf4e95c6aeae8cd3abe916
Patrick McClory <patrick@mcclory.io>
2020-11-12 11:42:57 -0800

    updates to change url calc for encrypted fields

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Django Dynamic Storages
 
 I'll fill this in with more detail later, but the basic reasoning for this project is that I needed something I could use to extend the already excellent [django-storages](https://github.com/jschneier/django-storages) project such that the storage field on any given `FileField` was a callable I could source a model instance value from. I've also included an abstract class for holding a configuration that stores (securely via [django-fernet-fields](https://github.com/orcasgit/django-fernet-fields)) the access configuration for a given storage backend as well.  
+
+## Revisions
+
+* `0.3.4` (Thu Nov 12 11:42:21 AM PST 2020) - minor update to allow for a callable function to be passed in for an `EncryptedFileField` so that you can externally control the view/url that needs to be used to get the contents of this file.
--- dynamic_storages/fields/encrypted_content.py
+++ dynamic_storages/fields/encrypted_content.py
@@ -9,17 +9,16 @@ from django.db.models.fields.files import FieldFile, FileField, ImageField
 from django.urls import reverse
 
 from ..conf import settings
-from .dynamic_storage import (DynamicStorageFieldFile, DynamicStorageFileField,
-                              DynamicStorageImageField,
-                              DynamicStorageImageFieldFile, FileFieldMixin)
+from .dynamic_storage import DynamicStorageFieldFile, DynamicStorageFileField, DynamicStorageImageField, DynamicStorageImageFieldFile, FileFieldMixin
 
 log = logging.getLogger(__name__)
 
 
 class EncryptedFile(BytesIO):
     """Wrapper class which encrypts the content supplied with the fernet specified on initialization"""
+
     def __init__(self, content, fernet):
-        if content and hasattr(content, 'read'):
+        if content and hasattr(content, "read"):
             content = content.read()
         if content:
             BytesIO.__init__(self, fernet.encrypt(content))
@@ -29,8 +28,9 @@ class EncryptedFile(BytesIO):
 
 class DecryptedFile(BytesIO):
     """Wrapper class around `io.BytesIO` that augments the initialization of the class to decrypt contents if a fernet is passed in to the constructor"""
+
     def __init__(self, content, fernet):
-        if content and hasattr(content, 'read'):
+        if content and hasattr(content, "read"):
             content = content.read()
         if content:
             BytesIO.__init__(self, fernet.decrypt(content))
@@ -41,9 +41,10 @@ class DecryptedFile(BytesIO):
 class EncryptedFieldFile(DynamicStorageFieldFile):
     def __init__(self, instance, field, name, **kwargs):
         self.fernet = getattr(field, "fernet", None)
+        self._get_url = getattr(field, "get_url", None)
         super().__init__(instance, field, name)
         if callable(self.fernet):
-            log.debug('EncryptedFieldFile init - fernet {} is callable'.format(self.fernet))
+            log.debug("EncryptedFieldFile init - fernet {} is callable".format(self.fernet))
             self.fernet = self.fernet(instance)
         if not self.fernet:
             raise RuntimeError("Encryption Fernet details not provided for this instance of a EncryptedFieldFile")
@@ -56,24 +57,16 @@ class EncryptedFieldFile(DynamicStorageFieldFile):
     def fernet(self, val):
         self._fernet = val
 
-    def _get_url(self):
-        if settings.DS_DOWNLOAD_URL_NAME:
-            url_args = []
-            for k in settings.DS_DOWNLOAD_URL_ARG_NAMES:
-                val = str(getattr(self.instance, k, None))
-                if val:
-                    url_args.append(val)
-            return reverse(settings.DS_DOWNLOAD_URL_NAME, args=url_args)
-        return None
-
     def get_decrypted(self):
         if self.fernet:
-            log.debug('Fernet type is {}'.format(type(self.fernet)))
-            return DecryptedFile(self.open('rb'), self.fernet)
+            log.debug("Fernet type is {}".format(type(self.fernet)))
+            return DecryptedFile(self.open("rb"), self.fernet)
         else:
-            return BytesIO(self.open('rb').read())
+            return BytesIO(self.open("rb").read())
 
-    url = property(_get_url)
+    @property
+    def url(self):
+        return self._get_url(self.instance) if self._get_url else None
 
     def deconstruct(self):
         name, path, args, kwargs = super().deconstruct()
